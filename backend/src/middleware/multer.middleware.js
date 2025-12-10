6cddc2a3435274ffe48ff45cf87fb0125aa25ee0
Manmeet Kaur <149240653+ManmeetKaur-28@users.noreply.github.com>
2025-12-11 01:41:51 +0530

    fiexed path in multer

--- backend/src/middleware/multer.middleware.js
+++ backend/src/middleware/multer.middleware.js
@@ -1,9 +1,9 @@
 import multer from "multer";
 import path from "path";
-// const uploadPath = path.join(process.cwd(), "public", "temp");
+const uploadPath = path.join(process.cwd(), "public", "temp");
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        return cb(null, "./public/temp");
+        return cb(null, uploadPath);
     },
     filename: function (req, file, cb) {
         return cb(null, `${req.user?._id}_${file.originalname}`);
