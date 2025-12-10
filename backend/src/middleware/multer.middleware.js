2816fa367d856633775fd77fa69a42cdbcbca954
Manmeet Kaur <149240653+ManmeetKaur-28@users.noreply.github.com>
2025-12-11 00:53:24 +0530

    added path import

--- backend/src/middleware/multer.middleware.js
+++ backend/src/middleware/multer.middleware.js
@@ -1,4 +1,5 @@
 import multer from "multer";
+import path from "path";
 const uploadPath = path.join(process.cwd(), "public", "temp");
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
